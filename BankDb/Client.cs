af1cd7b575e481dfaebc8bd8e99aba04d7f8234b
GraceTorresLeite <grace.torresleite@gmail.com>
2021-03-07 21:12:35 -0300

    add method test from csv

---
+++ BankDb/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDb
+{
+    public class Client
+    {
+        private int Id;
+        private string Nome;
+        private string Email;
+        private DateTime Nascimento;
+        private string Genero;
+        private string Estado_Civil;
+        public IList<Client> _clients = new List<Client>();
+
+        public Client(int id, string nome, string email, DateTime nascimento, string genero, string estado_Civil)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Nascimento = nascimento;
+            Genero = genero;
+            Estado_Civil = estado_Civil;
+        }
+
+        public Client()
+        {
+
+        }
+
+
+        public Client ValidaClient(int id)
+        {
+            var newClient = new Client();
+            if (id == this.Id)
+            {
+                throw new InvalidOperationException("This ID already contain in database");
+            }
+            else
+            {
+                return newClient;
+            }            
+        }
+    }
+}
--- BankDbTests/MathsTests.cs
+++ BankDbTests/MathsTests.cs
@@ -1,6 +1,8 @@
 ﻿using BankDb;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Drawing;
+using System.Linq;
 
 namespace BankDbTests
 {
@@ -13,7 +15,7 @@ namespace BankDbTests
             get { return testContextInstance; }
             set { testContextInstance = value; }
         }
-        
+
         [DataSource(@"Provider=MSOLEDBSQL;Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MathsData;Integrated Security=SSPI;", "AddIntegersData")]
         [TestMethod]
         public void AddIntegers_FromDataSourceTest()
@@ -29,9 +31,37 @@ namespace BankDbTests
                 "x:<{0}> y:<{1}>",
               new object[] { x, y });
 
-           // Assert.AreEqual(expected, actual);
+            // Assert.AreEqual(expected, actual);
 
             //Console.WriteLine(expected + ":" + actual);
+
+        }
+
+        
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+         "|DataDirectory|\\clients.csv", "clients#csv",
+          DataAccessMethod.Sequential), DeploymentItem("Data\\clients.csv")]
+        [TestMethod]
+        public void MigrationCSV()
+        {
+
+           
+           Console.WriteLine(TestContext.DataRow["Id"].ToString());
+            //int id = (int)TestContext.DataRow["Id"];
+            //string name = (TestContext.DataRow["Nome"].ToString());
+            //string email = (TestContext.DataRow["Email"].ToString());
+            //DateTime birth = (DateTime)TestContext.DataRow["Nascimento"];
+            //string gender = (TestContext.DataRow["Genero"].ToString());
+            //string civilState = (TestContext.DataRow["Estado Civil"].ToString());
+            //client.ValidaClient(id);
+            //client = new Client(id,name,email,birth,gender,civilState);
+
+
+            //var numberClientsExpected = 2;
+            //var numberClientsActual = client._clients.Count();
+
+            //Console.WriteLine(client._clients);
+            //Assert.AreEqual(numberClientsExpected, numberClientsActual);
 
         }
     }
